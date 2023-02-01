ce26ba5f108ba93485d01cdaf942027c6c29fa60
Nir Huri <hurinir@gmail.com>
2023-02-01 02:10:32 +0200

    kafka client

--- i-job/apps/api-gateway/src/auth/auth.service.ts
+++ i-job/apps/api-gateway/src/auth/auth.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @Inject('AUTH_MICROSERVICE') private readonly authClient: ClientKafka
+  ) {}
+
   getData(): { message: string } {
     return { message: 'Welcome to api-gateway!' };
   }
