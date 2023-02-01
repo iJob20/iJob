96f366bc7286126c0d23c2870187f6b9303e6e44
Nir Huri <hurinir@gmail.com>
2023-02-01 02:10:17 +0200

    initializing auth microservice with kafka

--- i-job/apps/auth/src/main.ts
+++ i-job/apps/auth/src/main.ts
@@ -5,18 +5,26 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: 'auth-consumer',
+        },
+      },
+    }
   );
+  app.listen();
 }
 
 bootstrap();
