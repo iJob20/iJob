35a52ca5b59209ebeaab0288a27e384722890267
Nir Huri <hurinir@gmail.com>
2023-02-05 00:00:57 +0200

    adding app to api-gateway

--- i-job/apps/api-gateway/src/main.ts
+++ i-job/apps/api-gateway/src/main.ts
@@ -6,10 +6,10 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
-import { AuthModule } from './auth/auth.module';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
+  const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
