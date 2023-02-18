a9c5c4ea6cff9ccb3404e11777e55442b1a4dde2
Nir Huri <hurinir@gmail.com>
2023-02-18 23:47:49 +0200

    some login page designs

---
+++ i-job/apps/ijob-client/src/app/views/LoginPage.tsx
@@ -0,0 +1,140 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Form,
+  Input,
+  InputGroup,
+  InputGroupText,
+  InputGroupAddon,
+  Container,
+  Col,
+} from 'reactstrap';
+import backgroundImage from '../assets/img/login.jpg';
+import logo from '../assets/img/now-logo.png';
+import TransparentFooter from '../component/Footer/TransparentFooter';
+import ExamplesNavbar from '../component/NavBar/ExamplesNavbar';
+
+function LoginPage() {
+  const [emailFocus, setEmailFocus] = useState(false);
+  const [passwordFocus, setPasswordFocus] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.add('login-page');
+    document.body.classList.add('sidebar-collapse');
+    document.documentElement.classList.remove('nav-open');
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove('login-page');
+      document.body.classList.remove('sidebar-collapse');
+    };
+  }, []);
+
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="page-header clear-filter" filter-color="blue">
+        <div
+          className="page-header-image"
+          style={{
+            backgroundImage: 'url(' + backgroundImage + ')',
+          }}
+        ></div>
+        <div className="content">
+          <Container>
+            <Col className="ml-auto mr-auto" md="4">
+              <Card className="card-login card-plain">
+                <Form action="" className="form" method="">
+                  <CardHeader className="text-center">
+                    <div className="logo-container">
+                      <img alt="..." src={logo}></img>
+                    </div>
+                  </CardHeader>
+                  <CardBody>
+                    <InputGroup
+                      className={
+                        'no-border input-lg' +
+                        (emailFocus ? ' input-group-focus' : '')
+                      }
+                    >
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="now-ui-icons users_circle-08"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        placeholder="First Name..."
+                        type="text"
+                        onFocus={() => setEmailFocus(true)}
+                        onBlur={() => setEmailFocus(false)}
+                      ></Input>
+                    </InputGroup>
+                    <InputGroup
+                      className={
+                        'no-border input-lg' +
+                        (passwordFocus ? ' input-group-focus' : '')
+                      }
+                    >
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="now-ui-icons text_caps-small"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        placeholder="Last Name..."
+                        type="text"
+                        onFocus={() => setPasswordFocus(true)}
+                        onBlur={() => setPasswordFocus(false)}
+                      ></Input>
+                    </InputGroup>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button
+                      block
+                      className="btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                      size="lg"
+                    >
+                      Get Started
+                    </Button>
+                    <div className="pull-left">
+                      <h6>
+                        <a
+                          className="link"
+                          href="#pablo"
+                          onClick={(e) => e.preventDefault()}
+                        >
+                          Create Account
+                        </a>
+                      </h6>
+                    </div>
+                    <div className="pull-right">
+                      <h6>
+                        <a
+                          className="link"
+                          href="#pablo"
+                          onClick={(e) => e.preventDefault()}
+                        >
+                          Need Help?
+                        </a>
+                      </h6>
+                    </div>
+                  </CardFooter>
+                </Form>
+              </Card>
+            </Col>
+          </Container>
+        </div>
+        <TransparentFooter />
+      </div>
+    </>
+  );
+}
+
+export default LoginPage;
