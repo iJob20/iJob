004e33025d0086dc3c8d7fee4c3e0f838de3f1d9
Nir Huri <hurinir@gmail.com>
2023-03-05 23:24:10 +0200

    changing tag-service to listen as microservice. also removing validationpipe from microservice controller

--- i-job/apps/tag-service/src/main.ts
+++ i-job/apps/tag-service/src/main.ts
@@ -5,18 +5,26 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  MicroserviceOptions,
+  TcpOptions,
+  Transport,
+} from '@nestjs/microservices';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.TAG_SERVICE_PORT || 3337;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: process.env.TAG_SERVICE_PORT || 3337,
+      },
+    } as TcpOptions
   );
+  app.listen();
 }
 
 bootstrap();
